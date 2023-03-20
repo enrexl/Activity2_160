d09a8fa05ea48ca6d08d363ffe3e42ca791ae9fe
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-03-20 15:20:13 +0700

    Membuat perulangan mengisi array

--- Latihan4ii_160/Latihan4ii_160.cpp
+++ Latihan4ii_160/Latihan4ii_160.cpp
@@ -4,6 +4,12 @@ using namespace std;
 int main()
 {
     int a[5];
-     
+    
+    cout << "Mengisi Array" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "Data ke - " << i << ": ";
+        cin >> a[i];
+
+    }
 }
 
